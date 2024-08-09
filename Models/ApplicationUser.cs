b82c70c608cb4e5c5da210ee7efe0a49a6638230
Arunz311 <arunachary88@gmail.com>
2024-08-09 11:26:08 +0530

    Add files via upload

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace StrangersChat2.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public string Address { get; set; } = "";
+        public DateTime CreatedAt { get; set; }
+        
+
+
+    }
+}
---
+++ Models/SmtpSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace StrangersChat2.Models
+{
+    public class SmtpSettings
+    {
+        public string Server { get; set; } = " ";
+        public int Port { get; set; }
+        public string Username { get; set; } = " ";
+        public string Password { get; set; } = " ";
+        public bool EnableSsl { get; set; }
+    }
+}
+
+
